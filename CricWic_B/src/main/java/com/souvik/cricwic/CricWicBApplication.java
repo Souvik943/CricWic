ae74223b93f726708baf90ecb5a940886daaf1eb
souvik943 <souvikkarmakar2k19@gmail.com>
2024-05-17 11:21:48 +0530

    Initial Commit

---
+++ CricWic_B/src/main/java/com/souvik/cricwic/CricWicBApplication.java
@@ -0,0 +1,13 @@
+package com.souvik.cricwic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricWicBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricWicBApplication.class, args);
+	}
+
+}
---
+++ CricWic_B/src/test/java/com/souvik/cricwic/CricWicBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souvik.cricwic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricWicBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
